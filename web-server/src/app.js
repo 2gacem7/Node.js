9226cff830ff9fc058f9e0981627596d95389b67
2gacem7 <gacemcode@gmail.com>
2021-08-24 23:22:00 +0200

    [WIP] Handlebars engine and views location

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,45 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
+// Define path for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engine and views location
+app.set('view engine','hbs')
+
+app.set('views', viewsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.render('index', {
+        title:'Weather app',
+        name:'Gacem BA'
+    })
+}) 
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title:'About me',
+        name:'Gacem Ba'
+    })
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
-})
-
-app.get('/about', (req, res) => {
-    res.send('About page')
+    res.render('help', {
+        message : 'I need a web developer job'
+    })
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
+    res.send({
+        forecast : 32,
+        location : 'Paris'
+    })
 })
 
 app.listen(3000, () => {
