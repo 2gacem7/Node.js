7e8db2bc325879c7dece14591acb0f4035510fa5
2gacem7 <gacemcode@gmail.com>
2021-08-29 22:54:12 +0200

    [WIP] Search bar created

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,25 @@
 console.log('Client side js file is loaded!');
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = 'Loading...'
+messageTwo.textContent = ''
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    fetch('http://localhost:3000/weather?adress=' + location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent =  data.forecast
+            }
+        })
+    })
+})
