2836e23391361c5eee1167c77597920bc40fef8a
2gacem7 <gacemcode@gmail.com>
2021-09-04 18:18:33 +0200

    [WIP] Mongoose, Reading Endpoint

--- task-manager/src/router/user.js
+++ task-manager/src/router/user.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 router.use(bodyParser.json());
 
-router.post("/users", (req, res) => {
+router.post('/users', (req, res) => {
     const user = new User(req.body)
     user.save().then(() => {
         res.status(201).send(user)
@@ -14,5 +14,27 @@ router.post("/users", (req, res) => {
         res.send(e)
     })
 });
+
+router.get('/users/:id', (req, res)=> {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e)=> {
+        res.status(500).send()
+    })
+})
+
+router.get('/users', (req, res) => {
+    User.find({}).then((users)=> {
+        res.send(users)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
 
 module.exports = router
